8daee19e08f9ffa070e5662add56058b0a8cb2e8
Alfred Wemali <alfredwemali05@gmail.com>
2023-06-30 17:23:13 +0300

    Textfield widget

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -28,7 +29,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpensesOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const Text('Add items to expenses'),
+      builder: (ctx) => const NewExpense(),
     );
   }
 
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            decoration: InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
